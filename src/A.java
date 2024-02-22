8a0b23a21509acd66e25b5d2ced8c8357e088e2e
Raghvendra Singh <147702713+Raghav20230@users.noreply.github.com>
2024-02-22 19:43:21 +0530

    second commit

--- src/A.java
+++ src/A.java
@@ -1,12 +1,13 @@
  class A {
          public static void main(String[] args) {
-        int[]x = new int[6];
+        int[]x = new int[10];
         x[0]=10;
         x[1]=22;
         x[2]=33;
         x[3]=44;
         x[4]=54;
         x[5]=66;
+        x[6]=99;
 //        x[2.5]=90;
 //        x[6]=7;
              System.out.println(x.length);
